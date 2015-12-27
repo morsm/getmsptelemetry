986d7eb3d0c71e2c26e4fde0fd2a8862a3216384
Maarten ter Mors <maarten@maartens-macbook.local>
2015-12-27 15:38:25 +0100

    Initial Commit

---
+++ get_msp_telemetry/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  get_msp_telemetry
+//
+//  Created by Maarten ter Mors on 27-12-15.
+//  Copyright © 2015 Morse Digital Media. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
